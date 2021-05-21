51bc2471a631871186a197750e1eaa2f235f5a4d
ateenrau <tttrung239@gmail.com>
2021-05-21 10:33:51 +0700

    add google

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://avclogging.herokuapp.com",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '0ccd4c2467ff3df7bca17c3401e100c3'),
+      secret: env("ADMIN_JWT_SECRET", "0ccd4c2467ff3df7bca17c3401e100c3"),
     },
   },
 });
